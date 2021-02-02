ad6494f7ff035c1456a1f919266221d6524fab67
TeguhSaputera <saputeraSV@gmail.com>
2021-02-02 15:34:54 -0800

    menambhakan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello World</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque reprehenderit impedit, aut expedita soluta deleniti totam nostrum numquam corporis eum, repellendus eligendi incidunt eius repudiandae necessitatibus debitis alias quam. Excepturi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+</body>
+</html>
